76c193a3610ef02d45a6acde9bb244ebcaf4fba4
FatmaMahmoudR <mahmoudfatma3060@gmail.com>
2024-09-30 23:36:51 +0300

    identity setup

---
+++ OnlineLearningPlatform/Models/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineLearningPlatform.Models
+{
+    public class AppUser : IdentityUser
+    {
+        [StringLength(100)]
+        public string? Address { get; set; }
+    }
+}
